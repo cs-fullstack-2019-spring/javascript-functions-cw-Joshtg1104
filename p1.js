83bb4fcaadfd563dc3955e30a965d7762470b6fb
Joshtg1104 <joshtg1104@gmail.com>
2019-01-13 23:34:20 -0600

    Gotta do this work

---
+++ p1.js
@@ -0,0 +1,17 @@
+// ### Problem 1
+// Using the code below:
+//     ```javascript
+// printNumbers();
+// ```
+// Create a ```printNumbers``` function to print integers from -25 to 20.
+
+function printNumbers(){
+    var x = -25;
+    while(x <= 20){
+        console.log(x);
+        x++;
+    }
+
+}
+
+printNumbers()
---
+++ p2.js
@@ -0,0 +1,10 @@
+// ### Problem 2
+// Create a function that’s passed a firstName and lastName. Return lastName comma firstName.
+
+function firstName_lastName() {
+    var firstName = prompt("Enter First Name");
+    var lastName = prompt("Enter Last Name");
+    console.log(lastName + ", " + firstName);
+}
+
+firstName_lastName()
